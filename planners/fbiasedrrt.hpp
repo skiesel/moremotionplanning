1fe01c97e62a5518a9db248b3f04238217c23c65
skiesel <skiesel@cs.unh.edu>
2015-12-18 15:49:58 -0500

    first swing at getting the old planners into this framework

---
+++ planners/fbiasedrrt.hpp
@@ -0,0 +1,192 @@
+#ifndef OMPL_CONTROL_PLANNERS_FBIASEDRRT_
+#define OMPL_CONTROL_PLANNERS_FBIASEDRRT_
+
+#include "ompl/control/planners/PlannerIncludes.h"
+#include "ompl/datastructures/NearestNeighbors.h"
+#include "ompl/base/goals/GoalSampleableRegion.h"
+#include "ompl/tools/config/SelfConfig.h"
+#include "../samplers/fbiasedstatesampler.hpp"
+#include <limits>
+
+namespace ompl {
+
+namespace control {
+
+class FBiasedRRT : public ompl::control::RRT {
+public:
+
+	/** \brief Constructor */
+	FBiasedRRT(const SpaceInformationPtr &si, double omega, double stateRadius) : ompl::control::RRT(si), omega(omega), stateRadius(stateRadius) {
+		setName("FBiased RRT");
+	}
+
+	virtual ~FBiasedRRT() {}
+
+	/** \brief Continue solving for some amount of time. Return true if solution was found. */
+	virtual base::PlannerStatus solve(const base::PlannerTerminationCondition &ptc) {
+		checkValidity();
+		base::Goal                   *goal = pdef_->getGoal().get();
+		base::GoalSampleableRegion *goal_s = dynamic_cast<base::GoalSampleableRegion *>(goal);
+
+		while(const base::State *st = pis_.nextStart()) {
+			Motion *motion = new Motion(siC_);
+			si_->copyState(motion->state, st);
+			siC_->nullControl(motion->control);
+			nn_->add(motion);
+		}
+
+		if(nn_->size() == 0) {
+			OMPL_ERROR("%s: There are no valid initial states!", getName().c_str());
+			return base::PlannerStatus::INVALID_START;
+		}
+
+		if(!sampler_) {
+			sampler_ = ompl::base::ValidStateSamplerPtr(new ompl::base::FBiasedStateSampler((ompl::base::SpaceInformation *)siC_, pdef_->getStartState(0), pdef_->getGoal(),
+				omega, stateRadius));
+			// sampler_ = si_->allocStateSampler();
+		}
+		if(!controlSampler_)
+			controlSampler_ = siC_->allocDirectedControlSampler();
+
+		OMPL_INFORM("%s: Starting planning with %u states already in datastructure", getName().c_str(), nn_->size());
+
+		Motion *solution  = NULL;
+		Motion *approxsol = NULL;
+		double  approxdif = std::numeric_limits<double>::infinity();
+
+		Motion      *rmotion = new Motion(siC_);
+		base::State  *rstate = rmotion->state;
+		Control       *rctrl = rmotion->control;
+		base::State  *xstate = si_->allocState();
+
+		while(ptc == false) {
+			/* sample random state (with goal biasing) */
+			if(goal_s && rng_.uniform01() < goalBias_ && goal_s->canSample())
+				goal_s->sampleGoal(rstate);
+			else {
+				sampler_->sample(rstate);
+			}
+
+			/* find closest state in the tree */
+			Motion *nmotion = nn_->nearest(rmotion);
+
+			/* sample a random control that attempts to go towards the random state, and also sample a control duration */
+			unsigned int cd = controlSampler_->sampleTo(rctrl, nmotion->control, nmotion->state, rmotion->state);
+
+			if(addIntermediateStates_) {
+				// this code is contributed by Jennifer Barry
+				std::vector<base::State *> pstates;
+				cd = siC_->propagateWhileValid(nmotion->state, rctrl, cd, pstates, true);
+
+				if(cd >= siC_->getMinControlDuration()) {
+					Motion *lastmotion = nmotion;
+					bool solved = false;
+					size_t p = 0;
+					for(; p < pstates.size(); ++p) {
+						/* create a motion */
+						Motion *motion = new Motion();
+						motion->state = pstates[p];
+						//we need multiple copies of rctrl
+						motion->control = siC_->allocControl();
+						siC_->copyControl(motion->control, rctrl);
+						motion->steps = 1;
+						motion->parent = lastmotion;
+						lastmotion = motion;
+						nn_->add(motion);
+						double dist = 0.0;
+						solved = goal->isSatisfied(motion->state, &dist);
+						if(solved) {
+							approxdif = dist;
+							solution = motion;
+							break;
+						}
+						if(dist < approxdif) {
+							approxdif = dist;
+							approxsol = motion;
+						}
+					}
+
+					//free any states after we hit the goal
+					while(++p < pstates.size())
+						si_->freeState(pstates[p]);
+					if(solved)
+						break;
+				} else
+					for(size_t p = 0 ; p < pstates.size(); ++p)
+						si_->freeState(pstates[p]);
+			} else {
+				if(cd >= siC_->getMinControlDuration()) {
+					/* create a motion */
+					Motion *motion = new Motion(siC_);
+					si_->copyState(motion->state, rmotion->state);
+					siC_->copyControl(motion->control, rctrl);
+					motion->steps = cd;
+					motion->parent = nmotion;
+
+					nn_->add(motion);
+					double dist = 0.0;
+					bool solv = goal->isSatisfied(motion->state, &dist);
+					if(solv) {
+						approxdif = dist;
+						solution = motion;
+						break;
+					}
+					if(dist < approxdif) {
+						approxdif = dist;
+						approxsol = motion;
+					}
+				}
+			}
+		}
+
+		bool solved = false;
+		bool approximate = false;
+		if(solution == NULL) {
+			solution = approxsol;
+			approximate = true;
+		}
+
+		if(solution != NULL) {
+			lastGoalMotion_ = solution;
+
+			/* construct the solution path */
+			std::vector<Motion *> mpath;
+			while(solution != NULL) {
+				mpath.push_back(solution);
+				solution = solution->parent;
+			}
+
+			/* set the solution path */
+			PathControl *path = new PathControl(si_);
+			for(int i = mpath.size() - 1 ; i >= 0 ; --i)
+				if(mpath[i]->parent)
+					path->append(mpath[i]->state, mpath[i]->control, mpath[i]->steps * siC_->getPropagationStepSize());
+				else
+					path->append(mpath[i]->state);
+			solved = true;
+			pdef_->addSolutionPath(base::PathPtr(path), approximate, approxdif, getName());
+		}
+
+		if(rmotion->state)
+			si_->freeState(rmotion->state);
+		if(rmotion->control)
+			siC_->freeControl(rmotion->control);
+		delete rmotion;
+		si_->freeState(xstate);
+
+		OMPL_INFORM("%s: Created %u states", getName().c_str(), nn_->size());
+
+		return base::PlannerStatus(solved, approximate);
+	}
+
+protected:
+
+	/** \brief State sampler */
+	base::ValidStateSamplerPtr                     sampler_;
+	double omega, stateRadius;
+};
+
+}
+}
+
+#endif
---
+++ samplers/fbiasedshellstatesampler.hpp
@@ -0,0 +1,178 @@
+#include <unordered_set>
+#include <unordered_map>
+
+#include <ompl/base/samplers/UniformValidStateSampler.h>
+
+#include <ompl/datastructures/NearestNeighborsSqrtApprox.h>
+#include <ompl/datastructures/NearestNeighborsGNAT.h>
+#include <ompl/datastructures/NearestNeighborsGNATNoThreadSafety.h>
+
+#include "../domains/geometry/detail/FCLContinuousMotionValidator.hpp"
+
+#include "../structs/probabilitydensityfunction.hpp"
+#include "../structs/inplacebinaryheap.hpp"
+
+namespace ompl {
+
+namespace base {
+
+class FBiasedShellStateSampler : public ompl::base::FBiasedStateSampler {
+
+	struct ExtraData {
+		ExtraData() : inExteriorPDF(false), touched(false) {}
+		bool inExteriorPDF, touched;
+	};
+
+public:
+	FBiasedShellStateSampler(ompl::base::SpaceInformation *base, ompl::base::State *start, const ompl::base::GoalPtr &goal,
+    double omega, double stateRadius, double shellPreference, double shellDepth) :
+		FBiasedStateSampler(base, start, goal, omega, stateRadius, false), shellPreference(shellPreference), shellDepth(shellDepth) {
+
+    for(auto vertex : vertices) {
+      vertex->extraData = new ExtraData();
+    }
+
+    reached(start);
+	}
+
+	virtual ~FBiasedShellStateSampler() {
+    for(auto vertex : vertices) {
+      delete (ExtraData*)vertex->extraData;
+    }
+  }
+
+	virtual bool sample(ompl::base::State *state) {
+    Vertex *randomVertex;
+    if(randomNumbers.uniform01() < shellPreference) {
+      randomVertex = exterior.sample();
+    } else {
+      randomVertex = interior.sample();
+    }
+
+    ompl::base::ScopedState<> vertexState(globalAppBaseControl->getGeometricComponentStateSpace());
+    vertexState = randomVertex->state;
+
+    ompl::base::ScopedState<> fullState = globalAppBaseControl->getFullStateFromGeometricComponent(vertexState);
+
+    fullStateSampler->sampleUniformNear(fullState.get(), state, stateRadius);
+
+    return true;
+	}
+
+	virtual bool sampleNear(ompl::base::State *, const ompl::base::State *, const double) {
+		throw ompl::Exception("FBiasedShellStateSampler::sampleNear", "not implemented");
+		return false;
+	}
+
+  void reached(ompl::base::State *state) {
+    reached(state, shellDepth);
+  }
+
+protected:
+
+  inline bool wasTouched(Vertex* v) const {
+    return ((ExtraData*)v->extraData)->touched;
+  }
+
+  inline void setTouched(Vertex* v) {
+    ((ExtraData*)v->extraData)->touched = true;
+  }
+
+  inline bool isInExteriorPDF(Vertex *v) const {
+    return ((ExtraData*)v->extraData)->inExteriorPDF;
+  }
+
+  inline void setInExteriorPDF(Vertex *v, bool inExteriorPDF) {
+    ((ExtraData*)v->extraData)->inExteriorPDF = inExteriorPDF;
+  }
+
+  void reached(ompl::base::State *state, double shellDepth) {
+    //we have a local shellDepth passed in, and a object level shellDepth -- maybe we want to alter it during runtime?
+    struct node {
+      node(unsigned int id, unsigned int depth, double value) : id(id), value(value) {}
+
+      static bool pred(const node *a, const node *b) {
+        return a->value < b->value;
+      }
+      static unsigned int getHeapIndex(const node *r) {
+        return r->heapIndex;
+      }
+      static void setHeapIndex(node *r, unsigned int i) {
+        r->heapIndex = i;
+      }
+
+      unsigned int id, depth, heapIndex;
+      double value;
+    };
+
+    Vertex v(0);
+    v.state = state;
+    unsigned int start = nn->nearest(&v)->id;
+    if(wasTouched(vertices[start])) {
+      return;
+    }
+
+    InPlaceBinaryHeap<node, node> open;
+    std::unordered_set<unsigned int> closed;
+    std::unordered_map<unsigned int, node *> lookup;
+
+
+    lookup[start] = new node(start, 0, 0);
+
+    if(isInExteriorPDF(vertices[start])) {
+      auto el = exterior.remove(vertices[start]->pdfID);
+      if(el != NULL) {
+        el->getData()->pdfID = el->getId();
+      }
+    }
+
+    auto el = interior.add(vertices[start], vertices[start]->score);
+    vertices[start]->pdfID = el->getId();
+    setInExteriorPDF(vertices[start], false);
+    setTouched(vertices[start]);
+
+    open.push(lookup[start]);
+    while(!open.isEmpty()) {
+      node *current = open.pop();
+      closed.insert(current->id);
+
+      std::vector<unsigned int> kids = getNeighboringCells(current->id);
+      for(unsigned int kid : kids) {
+        if(closed.find(kid) != closed.end()) continue;
+
+        double newValue = current->value + getEdgeCostBetweenCells(current->id, kid);
+
+        if(newValue <= shellDepth /*|| current->depth == 0*/) {
+          if(lookup.find(kid) != lookup.end()) {
+            if(newValue < lookup[kid]->value) {
+              lookup[kid]->value = newValue;
+              lookup[kid]->depth = current->depth + 1;
+              open.siftFromItem(lookup[kid]);
+            }
+          } else {
+            lookup[kid] = new node(kid, current->depth + 1, newValue);
+            open.push(lookup[kid]);
+          }
+        }
+      }
+    }
+
+    for(auto entry : lookup) {
+      auto n = entry.first;
+      delete entry.second;
+      if(!(isInExteriorPDF(vertices[n]) || wasTouched(vertices[n]))) {
+        auto el = exterior.add(vertices[n], vertices[n]->score);
+        vertices[n]->pdfID = el->getId();
+        setInExteriorPDF(vertices[n], true);
+      }
+    }
+  }
+
+	ProbabilityDensityFunction<Vertex> interior, exterior;
+	double shellPreference, shellDepth;
+	ompl::RNG randomNumbers;
+};
+
+}
+
+}
