9f0075c931891498dd04d98e3a66f84639d66eed
skiesel <skiesel@cs.unh.edu>
2016-02-22 15:27:44 -0500

    holy bash scripting batman

---
+++ experiments/experiments.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+PROJECT_ROOT="${HOME}/gopath/src/github.com/skiesel/moremotionplanning"
+
+DATA_ROOT="${PROJECT_ROOT}/experiments/data"
+EXEC="${PROJECT_ROOT}/build/MoreMotionPlanning"
+
+SEARCH="${HOME}/gopath/src/github.com/skiesel/search"
+dfcmplt="$SEARCH/rdb/dfcmplt"
+pathattrs="$SEARCH/rdb/pathattrs"
+pathfor="$SEARCH/rdb/pathfor"
+withattrs="$SEARCH/rdb/withattrs"
+
+EXPORT_CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PROJECT_ROOT}/build; "
+
+source generalsettings.inc
+moreGeneralSettings moreGen
+
+while [ "$moreGen" = true ];
+do
+	getGeneralPathAttrs generalPathAttrs
+	getGeneralParamFile generalParamFile
+	moreGeneralSettings moreGen
+
+	for DOMAIN_INC in `find -f *.dom`;
+	do
+		source $DOMAIN_INC
+		moreDomainSettings moreDS
+
+		while [ "$moreDS" = true ];
+		do
+			getDomainPathAttrs domainPathAttrs
+			getDomainParamFile domainParamFile
+			moreDomainSettings moreDS
+
+			for PLANNER_INC in `find -f *.alg`;
+			do
+				source $PLANNER_INC
+				morePlannerSettings morePS
+
+				while [ "$morePS" = true ];
+				do
+					getPlannerPathAttrs plannerPathAttrs
+					getPlannerParamFile plannerParamFile
+					morePlannerSettings morePS
+
+					outputFile=`$pathfor $DATA_ROOT $domainPathAttrs $plannerPathAttrs $generalPathAttrs`
+
+					inputFile="printf \"$generalParamFile $domainParamFile $plannerParamFile Output ? ${outputFile}\n\""
+					echo "if [ ! -s $outputFile ]; then $EXPORT_CMD $inputFile | $EXEC; fi"
+
+					exit
+				done
+			done
+		done
+	done
+done
